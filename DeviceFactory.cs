01c9c818bb4c3616e2c1a4c8222bd0a970049c8c
VIVEK SONI <erviveksoni@gmail.com>
2018-12-13 16:14:24 +0530

    Added missed enteries in device behaviour

--- DeviceFactory.cs
+++ DeviceFactory.cs
@@ -17,20 +17,23 @@ namespace Simulator
             {
                 var name = deviceToken[0];
                 var isServiceElevator = bool.Parse(deviceToken[1]);
+                var isBlocked = bool.Parse(deviceToken[2]);
                 var hasFaultyAc = bool.Parse(deviceToken[3]);
                 var isBusy = bool.Parse(deviceToken[4]);
+                var isJerky = bool.Parse(deviceToken[5]);
+                var isOnAuxPower = bool.Parse(deviceToken[6]);
+                var isDSCMalfunctioning = bool.Parse(deviceToken[7]);
 
                 var behaviour = new DeviceBehaviour()
                 {
                     IsService = isServiceElevator,
                     HasFaultyACUnit = hasFaultyAc,
-                    IsBusy = isBusy
+                    IsBusy = isBusy,
+                    IsBlocked = isBlocked,
+                    IsDSCMalfunctioning = isDSCMalfunctioning,
+                    IsJerky = isJerky,
+                    IsOnAuxPower = isOnAuxPower
                 };
-
-                #region BlockedBehaviour
-                //var isBlocked = bool.Parse(deviceToken[2]);
-                //behaviour.IsBlocked = isBlocked;
-                #endregion
 
                 var device = new Device(name, behaviour, this.transport);
 
